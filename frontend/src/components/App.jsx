e233ed2db5b5ef2d3e7b7bc4660488599e085647
Andrew_Walker <gulyaykin@gmail.com>
2023-04-25 15:50:12 +0300

    init: start project

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,3 @@
+const App = () => <h1>go</h1>;
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable functional/no-expression-statements */
+import ReactDOM from 'react-dom/client';
+import init from './init.jsx';
+
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(await init());
+};
+
+app();
